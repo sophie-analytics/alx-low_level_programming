13a423d5f38bbd3e853dd998aff04de7bb22d79b
Sophie-analytics <sonibaresofiat@gmail.com>
2023-09-07 10:11:20 -0700

    added a code that prints a string using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main -Entry Point
+ *
+ * Description: Prints a string using printf function
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+
+	return (0);
+}
