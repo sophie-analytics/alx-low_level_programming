6111d06f3c2fcd21fec81f71117bebe4f8e01e89
Sophie-analytics <sonibaresofiat@gmail.com>
2023-09-07 09:23:16 -0700

    added proper indentation

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main -Entry Point
+ * main - Entry Point
  * 
  * Description: Prints a string using the 'puts' function
  * Return: Always 0 (success)
- * */
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	
-	return 0;
+    puts("\"Programming is like building a multilingual puzzle");
+
+    return (0);
 }
