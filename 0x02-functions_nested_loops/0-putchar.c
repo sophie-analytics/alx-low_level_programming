da9730ef222092ebff6877e02ebbe1d79f6d6e0c
Sophie-analytics <sonibaresofiat@gmail.com>
2023-09-12 14:12:05 -0700

    added a read me and created a function and used it in a source code

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints a message using a defined function in the header file
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is contains use of functions, loops and also nested loops in c programming
