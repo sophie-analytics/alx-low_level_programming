a8e38e45cbcccc8ec085a99f001dc3cd27f85a42
Sophie-analytics <sonibaresofiat@gmail.com>
2023-09-13 02:01:59 -0700

    added a file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - Function for printing absolute value
+ *
+ * @n: The integer it takes
+ *
+ * Return: -n, if less than 0, n if gretaer than 0
+ */
+int _abs(int n)
+{
+	if (n < 0)
+{
+	return (-n);
+}
+else
+{
+	return (n);
+}
+}
