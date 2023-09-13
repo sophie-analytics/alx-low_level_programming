7644466b8a5ffb205295ad7a15bdf08bc9ae7a25
Sophie-analytics <sonibaresofiat@gmail.com>
2023-09-13 08:25:36 -0700

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * times_table - function that prints times table
+ *
+ */
+void times_table(void)
+{
+	int i, j, product;
+
+	for (i = 0; i < 10; i++)
+{
+	for (j = 0; j < 10; j++)
+{
+	product = i * j;
+
+	if (j != 0)
+{
+	_putchar(' ');
+}
+	if (product < 10)
+{
+	_putchar(' ');
+}
+else
+{
+	_putchar('0' + (product / 10));
+}
+	_putchar('0' + (product % 10));
+
+	if (j < 9)
+{
+	_putchar(',');
+}
+}
+
+	_putchar('\n');
+}
+}
