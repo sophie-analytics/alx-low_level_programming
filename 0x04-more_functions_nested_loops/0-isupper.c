9d5c50f6c0b7b9606aa8a8f07b3927841eab80aa
Sophie-analytics <sonibaresofiat@gmail.com>
2023-09-14 11:48:41 -0700

    corrected a coding style error

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _isupper: function that checks for case of letter
- *
+ * _isupper - function that checks for case of letter
+ * @c: the character it checks
  * Return: 0 if it is false and 1 if it is true
  */
 int _isupper(int c)
