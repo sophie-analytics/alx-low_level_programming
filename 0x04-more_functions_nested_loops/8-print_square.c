afbadc37ee6b176594ef00eaf439b77a762ee85c
Sophie-analytics <sonibaresofiat@gmail.com>
2023-09-15 16:47:42 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - function that print square
+ * @size: the number it takes in
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+{
+	_putchar('\n');
+}
+else
+{
+	for (i = 0; i < size; i++)
+{
+	for (j = 0; j < size; j++)
+{
+	_putchar('#');
+}
+	_putchar('\n');
+}
+}
+}
