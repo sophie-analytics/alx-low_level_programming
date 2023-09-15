b862f64ac425d0afa66f5a03559d1710cba90186
Sophie-analytics <sonibaresofiat@gmail.com>
2023-09-15 16:01:01 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - function that prints a line
+ * @n: number it takes in
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+{
+	_putchar('\n');
+}
+else
+{
+	for (i = 0; i < n; i++)
+{
+	_putchar('_');
+}
+	_putchar('\n');
+}
+}
