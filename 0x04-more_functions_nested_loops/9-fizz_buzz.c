9ac7f0713faa91a2ee443ab658c46554455a7565
Sophie-analytics <sonibaresofiat@gmail.com>
2023-09-15 17:19:14 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Description: print number from 1 -100
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+{
+	if (i % 3 == 0 && i % 5 == 0)
+{
+	printf("FizzBuzz ");
+}
+else if (i % 3 == 0)
+{
+	printf("Fizz ");
+}
+else if (i % 5 == 0)
+{
+	printf("Buzz ");
+}
+	printf("%d ", i);
+}
+	printf("\n");
+
+	return (0);
+}
