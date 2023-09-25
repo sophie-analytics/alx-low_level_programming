3a3dd55c39916981aff4c3486e7147831cd65201
Sophie-analytics <sonibaresofiat@gmail.com>
2023-09-25 16:17:42 -0700

    print chessboard values

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -12,4 +12,9 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 
 char *_strpbrk(char *s, char *accept);
+
+void print_chessboard(char (*a)[8]);
+
+int _putchar(char c);
+
 #endif
