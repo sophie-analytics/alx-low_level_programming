1367a6c5fea1b3402c9f046f0729c4a65e9b9bd6
Sophie-analytics <sonibaresofiat@gmail.com>
2023-10-02 13:40:41 -0700

    use argc to count number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - A function
+ * @argv: The argument vector
+ * @argc: The argument count
+ * Return: The result
+ */
+int main(int argc, char *argv[]  __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
