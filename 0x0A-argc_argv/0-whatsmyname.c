1f2ec7110a693f032d50a20aa888e88cbce646e0
Sophie-analytics <sonibaresofiat@gmail.com>
2023-10-02 12:58:05 -0700

    program that prints a name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - A function
+ * @argv: The argument vector
+ * @argc: The argument count
+ * Return: The result
+ */
+int main(int argc, char* argv[])
+{
+	if (argc > 0)
+{
+	printf("%s\n", argv[0]);
+}
+	return (0);
+}
