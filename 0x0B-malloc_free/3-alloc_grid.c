34f205bb940f552fac01d0aca4c62587630cdbc6
Sophie-analytics <sonibaresofiat@gmail.com>
2023-10-05 15:34:07 -0700

    Function that creates a 2D array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,6 +19,11 @@ int **alloc_grid(int width, int height)
 	grid = (int **)malloc(height * sizeof(int *));
 	if (grid == NULL)
 {
+	for (i = 0; i < height; i++)
+{
+	free(grid[i]);
+}
+	free(grid);
 	return (NULL);
 }
 	for (i = 0; i < height; i++)
@@ -26,6 +31,11 @@ int **alloc_grid(int width, int height)
 	grid[i] = (int *)malloc(width * sizeof(int));
 	if (grid[i] == NULL)
 {
+	for (j = 0; j < i; j++)
+{
+	free(grid[j]);
+}
+	free(grid);
 	return (NULL);
 }
 	for (j = 0; j < width; j++)
