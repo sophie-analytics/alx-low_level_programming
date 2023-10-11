fff4b2755754cbd9a48f97172e9735b709c958c5
Sophie-analytics <sonibaresofiat@gmail.com>
2023-10-11 07:43:15 -0700

    prints the values in a structure

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,21 +14,18 @@ void print_dog(struct dog *d)
 	}
 	if (d->name == NULL)
 	{
-		printf("Name: (nil)");
+		printf("Name: (nil)\n");
 	}
 	else
 	{
 		printf("Name: %s\n", d->name);
 	}
 
-	if (d->age > 0)
-	{
 		printf("Age: %f\n", d->age);
-	}
 
 	if (d->owner == NULL)
 	{
-		printf("Owner: (nil)");
+		printf("Owner: (nil)\n");
 	}
 	else
 	{
