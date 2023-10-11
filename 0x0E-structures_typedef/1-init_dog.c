0e8c86ad0da31dcd9cdadd6b5eb0e05073091f10
Sophie-analytics <sonibaresofiat@gmail.com>
2023-10-11 06:35:53 -0700

    Define a new type struct using a function

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * init_dog -Function that defines a sturcture
+ * @d: Structure name
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
