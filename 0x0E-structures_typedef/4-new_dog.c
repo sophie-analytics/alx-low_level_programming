077c0be225a9d71c2b03694e981bb2aa62fc135c
sophie-analytics <sonibaresofiat@gmail.com>
2023-10-12 13:00:19 +0100

    create a new struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,29 +12,32 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog = malloc(sizeof(dog_t));
+	dog_t *newDog = (dog_t *)malloc(sizeof(dog_t));
 
-	if (new_dog == NULL)
+	if (newDog == NULL)
 	{
 		return (NULL);
 	}
 
-	new_dog->name = strdup(name);
+	newDog->name = (char *)malloc(strlen(name));
 
-	if (new_dog->name == NULL)
+	if (newDog->name == NULL)
 	{
-		free(new_dog->name);
+		free(newDog);
 		return (NULL);
 	}
+	strcpy(newDog->name, name);
 
-	new_dog->owner = strdup(owner);
-	if (new_dog->owner == NULL)
+	newDog->owner = (char *)malloc(strlen(owner));
+
+	if (newDog->owner == NULL)
 	{
-		free(new_dog->owner);
-		free(new_dog);
+		free(newDog->name);
+		free(newDog);
 		return (NULL);
 	}
-	new_dog->age = age;
+	strcpy(newDog->owner, owner);
+	newDog->age = age;
 
-	return (new_dog);
+	return (newDog);
 }
