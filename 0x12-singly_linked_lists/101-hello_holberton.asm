5ed575b552c49b24ca55be574dce5a6fcb15fe62
sophie-analytics <sonibaresofiat@gmail.com>
2023-10-19 19:07:15 +0100

    assembly code

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,9 @@
-section .data
-	hello db "Hello, Holberton", 10 ;
-
-section .text
-	global _start
-
-_start:
-	;
-	mov rdi, hello   ; Load the address of the string into the rdi (1st argument)
-	call printf      ;
-	; Exit the program
-	mov rax, 60      ; syscall: exit
-	xor rdi, rdi     ; status: 0
-	syscall          ; Invoke syscall
-
-section .extern
-	extern printf
+global   main
+	 extern	 printf
+main:
+	mov   edi,   format
+	xor   eax,   eax
+	call  printf
+	mov   eax,    0
+	ret
+format: db `Hello, Holberton\n`, 0
