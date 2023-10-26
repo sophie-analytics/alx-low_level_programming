f19d19dda1bfc079203cabf26519be1002f14dc1
sophie-analytics <sonibaresofiat@gmail.com>
2023-10-26 23:16:05 +0100

    converts a binary no to an unsigned int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * int binary_to_uint - Function that converts a binary no to an unsigned int
+ * @b: The no it takes
+ * Return: The unsigned int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int sum = 0;
+	int i;
+	int string_length = strlen(b);
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+
+	for (i = 0; i < string_length; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+		{
+			return (0);
+		}
+		sum = (sum << 1) + (b[i] - '0');
+	}
+	return (sum);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,6 @@
+#ifndef LISTS_H
+#define LIST_H
+
+unsigned int binary_to_uint(const char *b);
+
+#endif
