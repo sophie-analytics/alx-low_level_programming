093fc579463c80606dba949982f87a57a41cab65
sophie-analytics <sonibaresofiat@gmail.com>
2023-10-30 16:29:50 +0100

    corrected an indentation error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,8 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	file_descriptor = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	file_descriptor = open(filename, O_RDWR |
+	O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (file_descriptor == -1)
 	{
 		return (-1);
