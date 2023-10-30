b322bedd663fe95ffd7be1bc94ea81fd6dd816a6
sophie-analytics <sonibaresofiat@gmail.com>
2023-10-30 16:49:39 +0100

    added more error checks

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,6 +23,12 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
+	file_descriptor = open(filename, O_WRONLY);
+	if (file_descriptor == -1)
+	{
+		return (-1);
+	}
+	close(file_descriptor);
 
 	file_descriptor = open(filename, O_WRONLY | O_APPEND);
 	if (file_descriptor == -1)
