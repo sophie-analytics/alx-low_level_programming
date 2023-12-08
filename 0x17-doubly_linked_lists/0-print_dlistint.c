d795613755fa91f56ec6fbb5c1a82551468755a5
Sophie-analytics <sonibaresofiat@gmail.com>
2023-12-08 19:53:53 +0100

    fix: betty error

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,25 +1,22 @@
 #include "lists.h"
 #include <stdio.h>
-
-
 /**
  * print_dlistint - print the values in the linked list
  * @h: The head pointer it takes
  * Return: Number of elements in the linked list
  */
-
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t elements;
-    const dlistint_t* temp;
+size_t elements;
+const dlistint_t *temp;
 
-    elements = 0;
-    temp = h;
-    while(temp != NULL)
-    {
-        printf("%d\n", temp->n);
-        temp = temp->next;
-        elements++;
-    }
-    return elements;
+elements = 0;
+temp = h;
+while (temp != NULL)
+{
+printf("%d\n", temp->n);
+temp = temp->next;
+elements++;
+}
+return (elements);
 }
