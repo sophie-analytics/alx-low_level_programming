8c41b51b3cdc8002349fd7623744ea75b667e346
Sophie-analytics <sonibaresofiat@gmail.com>
2023-12-08 19:27:18 +0100

    added a read me file

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdio.h>
+
+
+/**
+ * print_dlistint - print the values in the linked list
+ * @h: The head pointer it takes
+ * Return: Number of elements in the linked list
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+    size_t elements;
+    const dlistint_t* temp;
+
+    elements = 0;
+    temp = h;
+    while(temp->next != NULL)
+    {
+        printf("%d\n", temp->n);
+        temp = temp->next;
+        elements++;
+    }
+    printf("%d\n", temp->n);
+    elements++;
+    return elements;
+}
